aa22443d2d51056557886108768792a112e4729f
Mitchell Johnson <mitchelljohnson@Mitchells-MacBook-Pro-3.local>
2020-04-21 21:23:14 -0600

    backend woworking

--- nypdBackend/api/views.py
+++ nypdBackend/api/views.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 from api.models import conviction
 from api.serializers import convictionSerializer
+import pyodbc
 
 
 #class getCSV(APIView):
@@ -23,20 +24,38 @@ class CreateConviction(APIView):
     @csrf_exempt
     def post(self, request, format=None):
         body = json.loads(request.body)
-        newSale = conviction()
-        newSale.ARREST_BORO = body["ARREST_BORO"]
-        newSale.ARREST_PRECINCT = body["ARREST_PRECINCT"]
-        newSale.JURISDICTION_CODE = body["JURISDICTION_CODE"]
-        newSale.AGE_GROUP = body["AGE_GROUP"]
-        newSale.PERP_SEX = body["PERP_SEX"]
-        newSale.PERP_RACE = body["PERP_RACE"]
-        newSale.DRUG_RELATED = body["DRUG_RELATED"]
-        newSale.LONGITUDE = body["LONGITUDE"]
-        newSale.WEEKDAY = body["WEEKDAY"]
-        newSale.save()
+        #newSale = conviction()
+        #newSale.ARREST_BORO = body["ARREST_BORO"]
+        #newSale.ARREST_PRECINCT = body["ARREST_PRECINCT"]
+        #newSale.JURISDICTION_CODE = body["JURISDICTION_CODE"]
+        #newSale.AGE_GROUP = body["AGE_GROUP"]
+        #newSale.PERP_SEX = body["PERP_SEX"]
+        #newSale.PERP_RACE = body["PERP_RACE"]
+        #newSale.DRUG_RELATED = body["DRUG_RELATED"]
+        #newSale.LONGITUDE = body["LONGITUDE"]
+        #newSale.WEEKDAY = body["WEEKDAY"]
+        #newSale.save()
+
+
+        server = 'isserver2020.database.windows.net'
+        database = 'NYPD'
+        username = 'jboy7977'
+        password = 'C00ties8119036!'
+        driver= '{ODBC Driver 17 for SQL Server}'
+        cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+        cursor = cnxn.cursor()
+        #cursor.execute("SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid")
+        cursor.execute("INSERT INTO FinalNYPDData (ARREST_BORO, ARREST_PRECINCT, JURISDICTION_CODE, AGE_GROUP, PERP_SEX, PERP_RACE, DRUG_RELATED, LONGITUDE, WEEKDAY)VALUES(" + body["ARREST_BORO"]+","+body["ARREST_PRECINCT"]+","+ body["JURISDICTION_CODE"]+","+body["AGE_GROUP"]+","+body["PERP_SEX"]+","+body["PERP_RACE"]+","+body["DRUG_RELATED"]+","+body["LONGITUDE"]+","+body["WEEKDAY"]+")")
+        #row = cursor.fetchone()
+        #while row:
+        #    print (str(row[0]) + " " + str(row[1]))
+        #    row = cursor.fetchone()
+
+
+
         return Response({
-            'ARREST_BORO': newSale.ARREST_BORO,
-            'DRUG_RELATED':newSale.DRUG_RELATED,
+            'ARREST_BORO': body["ARREST_BORO"],
+            'DRUG_RELATED':body["DRUG_RELATED"],
             })
 
     
